f5346a50eb7681af3677a84a0385ad2ab4f650d9
vikashuvaeva <vikashuvaeva112@gmail.com>
2022-04-28 20:58:14 +0300

    Класс Task для календаря

---
+++ TaskClass/TaskClass/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskClass
+{
+    public class Task // Задача - занятие с клиенткой
+    {
+        public string name //имя клиентки
+        public DateTime date //дата и время занятия
+        public DateTime duration //продолжительность занятия
+        public int cost //стоимость занятия
+        public string note //заметка
+
+    public Task(string customerName, DateTime customerDate, DateTime customerDuration, int customerCost, string customerNote)
+        {
+            name = customerName;
+            date = customerDate;
+            cost = customerCost;
+            duration = customerDuration;
+            note = customerNote;
+        }
+    }
+}
---
+++ TaskClass/TaskClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
